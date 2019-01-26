4ae68194cce30ea748f8cbdc598dc6c21cd306ce
Konstantin <konstantin@MacBook-Pro-Konstantin.local>
2019-01-26 17:39:53 +0300

    Byte 29 complete

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-cars = {
-    'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
-    'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
-    'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
-    'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
-    'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
-}
-
-for i in cars:
-    print(sorted(cars[i]))
+alphanumeric = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') + range(0,10)
+a = [1, 2, 3]
+a = int(a[0])
+print(a)
