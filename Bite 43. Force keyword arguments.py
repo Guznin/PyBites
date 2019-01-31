fa1e3655ffe3f3760f23f331ccb4b9fa6621b254
guznin <guznin@gmail.com>
2019-01-31 17:18:40 +0300

    43

---
+++ Bite 43. Force keyword arguments.py
@@ -0,0 +1,8 @@
+"""Write a function called get_profile that can only allows 2 keyword arguments: name and profession which default to julian and programmer respectively.
+
+The function does nothing fancy, just return a str: name is a profession.
+
+The point is to limit the interface of this function and you'll see Python makes it very easy ... have fun!"""
+
+
+def get_profile(...
