6f6fe8578660f3486692300c0857df9a13694fab
Konstantin <konstantin@MacBook-Pro-Konstantin.local>
2019-02-02 00:01:42 +0300

    45

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-import string, random
-alphabet = string.ascii_letters + string.digits
+import collections
 
-random.choices(alphabet, k=4)
+def my_queue(n=5):
+    mylist = collections.deque(maxlen=n)
+    
+    return [-1:]
+
+# my_queue(n=5)
+if __name__ == '__main__':
+    mq = my_queue()
+    for i in range(10):
+        mq.append(i)
+        print((i, list(mq)))
