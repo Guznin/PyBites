e2fa654d15964218b44bf3d8afcdd5f1212089b7
guznin <guznin@gmail.com>
2019-02-04 10:40:18 +0300

    54

--- Bite 44. License key generator.py
+++ Bite 44. License key generator.py
@@ -7,17 +7,24 @@ as gen_key(parts=3, chars_per_part=4) to get a shorter one, e.g.: 54N8-I70K-2JZ7
 Before you default to random, check if Python >= 3.6 has a better option available for this task ..."""
 import string, random
 
+ALPHABET = string.ascii_uppercase + string.digits
+
 
 def gen_key(parts=4, chars_per_part=8):
-    alphabet = string.ascii_letters.upper() + string.digits
     join = ''.join
     keylist = []
     add = keylist.append
-
     while len(keylist) < parts:
-        part_key = join(random.choices(alphabet, k=chars_per_part))
+        part_key = join(random.choices(ALPHABET, k=chars_per_part))
         add(part_key)
     return '-'.join(i for i in keylist)
 
 
 gen_key() 
+
+
+
+# DASH = '-'
+# def gen_key(parts=4, chars_per_part=8):
+#     return DASH.join(''.join(choice(ALPHABET) for i in range(chars_per_part))
+#                      for _ in range(parts))
