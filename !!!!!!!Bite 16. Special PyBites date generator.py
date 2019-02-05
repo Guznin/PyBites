e59b17ada159cbf4ec7930a163664fcd90349902
guznin <guznin@gmail.com>
2019-02-05 17:03:28 +0300

    fucking rouns of bank

--- !!!!!!!Bite 16. Special PyBites date generator.py
+++ !!!!!!!Bite 16. Special PyBites date generator.py
@@ -6,22 +6,16 @@ See the tests for more details how your code will be tested: as this is a beginn
 
 We will revisit this in an intermediate challenge. Have fun!"""
 
-import datetime
+from datetime import datetime, timedelta
 
-PYBITES_BORN = datetime.datetime(year=2016, month=12, day=19)
+
+
+PYBITES_BORN = datetime(year=2016, month=12, day=19)
 
 
 def gen_special_pybites_dates():
-    a = []
-    while PYBITES_BORN.year < 2017:
-        newdate = PYBITES_BORN.day + 100
-        a.append(newdate)
-    return a
-
-
-
-
-gen_special_pybites_dates()
-
-
-print('#'*200)
+    i = []
+    MAX_DATE = datetime(year=2020, month=12, day=31)
+    while PYBITES_BORN < MAX_DATE:
+        PYBITES_BORN =  PYBITES_BORN + timedelta(days=100)
+        i.appe
