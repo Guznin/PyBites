394c0aa4741a89e504958cbb05b37f93af75db95
guznin <guznin@gmail.com>
2019-02-06 13:42:05 +0300

    74

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-'!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.split()
+import calendar
+
+
+def weekday_of_birth_date(date):
+    """Takes a date object and returns the corresponding weekday string"""
+    # calendar is nice, but you can also use: date.strftime('%A')
+    weekday = date.weekday()
+    return calendar.day_name[weekday]
