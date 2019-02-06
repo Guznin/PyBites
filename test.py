c9c053344edd5e117a6deed50e4aa0bdf746ff69
guznin <guznin@gmail.com>
2019-02-06 16:58:46 +0300

    143

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-import calendar
+a = 'mamaLOWERCASE'
 
-
-def weekday_of_birth_date(date):
-    """Takes a date object and returns the corresponding weekday string"""
-    # calendar is nice, but you can also use: date.strftime('%A')
-    weekday = date.weekday()
-    return calendar.day_name[weekday]
+a.lower()
