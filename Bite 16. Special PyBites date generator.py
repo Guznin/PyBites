9a8bd8fbf37d14e4504684695ec3f68c1ba11863
guznin <guznin@gmail.com>
2019-02-07 14:23:49 +0300

    datetime

--- Bite 16. Special PyBites date generator.py
+++ Bite 16. Special PyBites date generator.py
@@ -8,14 +8,12 @@ We will revisit this in an intermediate challenge. Have fun!"""
 
 from datetime import datetime, timedelta
 
-
-
 PYBITES_BORN = datetime(year=2016, month=12, day=19)
 
 
 def gen_special_pybites_dates():
-    i = []
-    MAX_DATE = datetime(year=2020, month=12, day=31)
-    while PYBITES_BORN < MAX_DATE:
-        PYBITES_BORN =  PYBITES_BORN + timedelta(days=100)
-        i.appe
+    days = 0
+    while True:
+        days += 1
+        if days % 100 == 0 or days % 365 == 0:
+            yield PYBITES_BORN + timedelta(days=days)
--- test.py
+++ test.py
@@ -1,9 +1,5 @@
-my_cities = ['Rome', 'Paris', 'Madrid', 'Chicago', 'Seville', 'Berlin']
-other_cities = ['Rome', 'Paris', 'Madrid', 'Chicago', 'Seville', 'Berlin']
+a = 1
 
-
-def uncommon_cities(my_cities=my_cities, other_cities=other_cities):
-    """Compare my_cities and other_cities and return the number of different
-       cities between the two"""
-    my_set = set(my_cities) ^ set(other_cities)
-    return len(my_set)
+while a > 10:
+    print(a)
+    a = 1 + a
